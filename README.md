8c88302f1e2635d56f22920b96e3524ce25590cd
mansipatil391 <140091587+mansipatil391@users.noreply.github.com>
2023-08-02 18:32:40 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# SIMBT_04
-Created with CodeSandbox
+# E-Commerce Web Application using React JS & Tensorflow JS
+
+### Overview
+<ul>
+  <li>Dashboard style e-commerce website</li>
+  <li>Responsive layout</li>
+  <li>No CSS libraries</li>
+  <li>Using Styled Components</li>
+  <li>React Context API</li>
+  <li>Tensorflow MobileNet Image Classifier for searching purposes</li>
+</ul>
+
+### CodeSandBox Demo
+
+<a href="https://codesandbox.io/s/festive-chatterjee-ocwwu?file=/src/App.js" target="_blank"> Check this out </a>
+<br>
+All the style is written using styled-components in .js file.
+<br>
+Svg illustration are contributed by <a href="https://undraw.co/illustrations" target="_blank"> Undraw </a>
+<br>
+
+### Searching Process
+
+The search bar allows the input of product name, brand, colour and image address.
+<br>
+<br>
+While inputting product name, brand and colour, the webpage directly routes you to the product page, and the array of a specific item is being displayed.
+<br>
+<br>
+While inputting the image address, the image identification box directly pops up using a particular javascript regular expression. You can then press the identify image button; within the 20ms interference time of the MobileNet v2 model, an array of 3 prediction results are being displayed by the probability.
+<br>
+<br>
+There are particular image URL blocked by the cross-origin policy (CORS policy). The user can also provide an image file input to the browser for image classification.
+<br>
+
+### Brief Intro of Tensorflow JS MobileNet
+
+MobileNet-v2 is a convolutional neural network that is 53 layers deep. You can load a pre-trained version of the network trained on more than a million images from the ImageNet database. The pre-trained network can classify images into 1000 object categories, such as keyboard, mouse, pencil, and many animals. As a result, the network has learned rich feature representations for a wide range of images. 
+
+MobileNet-v2 image classification for javascript from Tensorflow JS library provides eye-opening interactivity for the users or clients without any installation. This is the model that runs directly in the browser, and the data input stays only at the client side; this significantly secures the privacy of the clients or users. Accurate results are provided for the users in the vast decrease in time(20ms interference time) and better performance experience than VGG-16(OxfordNet). The model can determine even the species of the animal.
+
---
+++ src/App.js
@@ -0,0 +1,138 @@
+import React, { useEffect, useState, useRef } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory,
+} from "react-router-dom";
+// import * as mobilenet from "@tensorflow-models/mobilenet";
+import styled from "styled-components";
+import allProducts from "./data/all";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import Explore from "./components/Explore";
+import Saved from "./components/Saved";
+import Cart from "./components/Cart";
+import ProductScreen from "./components/ProductScreen";
+import ScrollToTop from "./components/ScrollToTop";
+import Signup from "./components/Signup";
+import Aftersales from "./components/Aftersales";
+import Analytics from "./components/Analytics";
+
+function App() {
+  const history = useHistory();
+  const [search, setSearch] = useState("");
+  const [result, setResult] = useState([]);
+
+  const handleSearch = (input) => {
+    input = input.toLowerCase().trim();
+    setSearch(input);
+    const arr = input.split(" ");
+
+    var all = [];
+
+    var exact = allProducts?.filter((found) => {
+      var temp =
+        found.fullName + " " + found.id + " " + found.brand + " " + found.color;
+      temp = temp.toLowerCase();
+      return (
+        arr.every((item) => temp.includes(item)) ||
+        found.url.toLowerCase().includes(input.replace("http", ""))
+      );
+    });
+
+    all.push(...exact);
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].length < 2 || arr[i] === "I" || arr[i] === "to") {
+        // all = [];
+        continue;
+      }
+
+      var similar = allProducts?.filter((found) => {
+        return (
+          found.fullName.toLowerCase().includes(arr[i]) ||
+          found.color.toLowerCase().includes(arr[i]) ||
+          found.id.toLowerCase().includes(arr[i]) ||
+          found.brand.toLowerCase().includes(arr[i]) ||
+          found.url.toLowerCase().includes(input.replace("http", ""))
+        );
+      });
+      all.push(...similar);
+    }
+
+    all = [...new Set(all)];
+    // shuffle(all);
+    setResult([...all]);
+  };
+
+  return (
+    <Router>
+      <Container>
+        <Sidebar />
+        <SidebarOverlay></SidebarOverlay>
+        <Switch>
+          <Main>
+            <Header search={handleSearch} />
+            <HeaderOverlay></HeaderOverlay>
+
+            <Route path="/products/all" exact>
+              <Products
+                products={search?.length === 0 ? allProducts : result}
+                input={search}
+              />
+            </Route>
+            <ScrollToTop>
+              <Route path="/products/all/:id" exact>
+                <ProductScreen />
+              </Route>
+            </ScrollToTop>
+            <Route path="/explore">
+              <Explore />
+            </Route>
+            <Route path="/saved" exact>
+              <Saved />
+            </Route>
+            <Route path="/cart" exact>
+              <Cart />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+            <Route path="/aftersales">
+              <Aftersales />
+            </Route>
+            <Route path="/analytics">
+              <Analytics />
+            </Route>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+          </Main>
+        </Switch>
+      </Container>
+    </Router>
+  );
+}
+
+export default App;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 78px auto;
+  overflow-x: hidden;
+`;
+
+const Main = styled.div`
+  display: grid;
+  grid-template-rows: 50px auto;
+`;
+
+const SidebarOverlay = styled.div``;
+
+const HeaderOverlay = styled.div``;
